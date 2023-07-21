edffebcb047a0d788929c02967f5b6f4461e5db8
Domenic Pittari <dom.pittari@gmail.com>
2023-07-21 14:46:42 +0900

    Added friction

--- Project Lumina/Assets/Scripts/Player/PlayerMovement.cs
+++ Project Lumina/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,18 +7,30 @@ namespace ProjectLumina.Player
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField, Range(0, 25)]
-        private float _moveSpeed;
+        private float _moveSpeed, _velocity, _acceleration, _decceleration, _frictionAmount;
 
-        private Rigidbody2D _rigidbody;
+        private Rigidbody2D _rb;
 
         private void Awake()
         {
-            _rigidbody = GetComponent<Rigidbody2D>();
+            _rb = GetComponent<Rigidbody2D>();
         }
 
         public void Move(float move)
         {
-            _rigidbody.velocity = new Vector2(move * _moveSpeed, _rigidbody.velocity.y);
+            float targetSpeed = move * _moveSpeed;
+            float speedDiff = targetSpeed - _rb.velocity.x;
+            float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? _acceleration : _decceleration;
+            float movement = Mathf.Pow(Mathf.Abs(speedDiff) * accelRate, _velocity) * Mathf.Sign(speedDiff);
+
+            _rb.AddForce(movement * Vector2.right);
+
+            if (Mathf.Abs(move) < 0.01f)
+            {
+                float amount = Mathf.Min(Mathf.Abs(_rb.velocity.x), Mathf.Abs(_frictionAmount));
+                amount *= Mathf.Sign(_rb.velocity.x);
+                _rb.AddForce(Vector2.right * -amount, ForceMode2D.Impulse);
+            }
         }
     }
 }
