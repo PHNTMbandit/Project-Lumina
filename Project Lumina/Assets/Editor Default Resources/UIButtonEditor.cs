bb9a90e2e4cee4f30deb45d0ec51d97a055e2611
Domenic Pittari <dom.pittari@gmail.com>
2023-08-28 08:58:37 +0900

    Added interactable fade animation

---
+++ Project Lumina/Assets/Editor Default Resources/UIButtonEditor.cs
@@ -0,0 +1,17 @@
+using ProjectLumina.UI;
+using UnityEditor;
+using UnityEditor.UI;
+
+namespace ProjectLumina.Editor
+{
+    [CustomEditor(typeof(UIButton))]
+    public class UIButtonEditor : ButtonEditor
+    {
+        // public override void OnInspectorGUI()
+        // {
+        //     UIButton targetMyButton = (UIButton)target;
+
+        //     base.OnInspectorGUI();
+        // }
+    }
+}
